f080df76250612cddf742e04a125103c9bb8cba6
Bonny Philip <106816732+MihaliRahael@users.noreply.github.com>
2023-07-27 17:40:55 +0530

    Delete Savitzky_Golay_Filter.py

--- Savitzky_Golay_Filter.py
+++
@@ -1,7 +0,0 @@
-import yfinance as yf
-from scipy.signal import savgol_filter  # Download the daily stock-price history for Tesla
-df = yf.download('TSLA')  # Calculate the moving average and store as a new column
-df['MA'] = df['Close'].rolling(21).mean()
-df['sav_gol'] = savgol_filter(df['Close'], window_length = 21, polyorder = 5)  # Filter to the last 120 trading days and plot
-df = df[-120:]
-df.plot(y=['Close', 'MA', 'sav_gol'])
